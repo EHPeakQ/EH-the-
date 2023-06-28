81bdf95e77285de3c299d717c02d55cd0c98c29d
EHPeakQ <138035986+EHPeakQ@users.noreply.github.com>
2023-06-29 01:13:45 +0700

    Upload file hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+public class hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
